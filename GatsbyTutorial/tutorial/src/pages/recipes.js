e70136efc6303c3e53d47c357d6030a6e74d3989
Saadat <saadat@asl.aero>
2021-11-16 17:44:12 +0600

    house cleaning

--- GatsbyTutorial/tutorial/src/pages/recipes.js
+++ GatsbyTutorial/tutorial/src/pages/recipes.js
@@ -1,16 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { page, text } from "../examples/about.module.css"
-import styled from "styled-components"
 
 const recipes = () => {
   return (
     <Layout>
-      <div className={page}>
-        <h1>about page</h1>
-        <h1>Hello from about page</h1>
-        <p className={text}>lorem ipsum dfj fds;kljsdf fj;aklsdfa fsda;klj</p>
-      </div>
+      <h1>Recepies page</h1>
+      <h1>Hello from recepies page</h1>
+      <p>lorem ipsum dfj fds;kljsdf fj;aklsdfa fsda;klj</p>
     </Layout>
   )
 }
--- GatsbyTutorial/tutorial/src/pages/tags.js
+++ GatsbyTutorial/tutorial/src/pages/tags.js
@@ -1,16 +1,13 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { page, text } from "../examples/about.module.css"
 import styled from "styled-components"
 
 const tags = () => {
   return (
     <Layout>
-      <div className={page}>
-        <h1>about page</h1>
-        <h1>Hello from about page</h1>
-        <p className={text}>lorem ipsum dfj fds;kljsdf fj;aklsdfa fsda;klj</p>
-      </div>
+      <h1>Tags page</h1>
+      <h1>Hello from about page</h1>
+      <p>lorem ipsum dfj fds;kljsdf fj;aklsdfa fsda;klj</p>
     </Layout>
   )
 }
